f655ec0f5d2cd27a2f93b0dd017e73eb2ae83741
vardakisDev <stelios13vardakis@gmail.com>
2020-05-20 18:00:08 +0300

    fixes

--- scrapper.js
+++ scrapper.js
@@ -1,13 +1,14 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
+const request = require("request");
 
 const URL = "https://www.imdb.com/find?s=tt&ref_=fn_al_tt_mr&q=";
 const movieURL = "https://www.imdb.com/title/";
 
 const movieKeys = "/keywords?ref_=tt_stry_kw";
 
-function searchMovies(searchTerm) {
-  return fetch(`${URL}${searchTerm}`)
+async function searchMovies(searchTerm) {
+  return await fetch(`${URL}${searchTerm}`)
     .then((response) => response.text())
 
     .then((body) => {
@@ -33,25 +34,37 @@ function searchMovies(searchTerm) {
 }
 
 async function GetKeywords(imdbID) {
-  return fetch(`${movieURL}${imdbID}${movieKeys}`)
+  return await fetch(`${movieURL}${imdbID}${movieKeys}`)
     .then((response) => response.text())
     .then((body) => {
       const $ = cheerio.load(body);
       const keywords = [];
-      $("table > tbody > tr > td.soda.sodavote > div.sodatext > a").each((index, element) => {
-        keywords.push($(element).text());
-
+      $("table > tbody > tr > ").each((index, element) => {
+        let key = $(element)
+          .find("td.soda.sodavote > div.sodatext > a")
+          .text()
+          .trim();
+        let relevance = $(element)
+          .find(" div.did-you-know-actions > div > a")
+          .text()
+          .trim();
+        const keyword = {
+          key: key,
+          relevance: relevance,
+        };
+        keywords.push(keyword);
       });
 
       return keywords;
     });
 }
 
-function GetMovie(imdbID) {
-  return fetch(`${URL}${imdbID}`)
+async function GetMovie(imdbID) {
+  return await fetch(`${URL}${imdbID}`)
     .then((response) => response.text())
     .then((body) => {
       const $ = cheerio.load(body);
+      //title
       const $title = $(".title_wrapper h1");
       const title = $title
         .first()
@@ -61,39 +74,77 @@ function GetMovie(imdbID) {
         })
         .text()
         .trim();
+      //screentime and and year relased
+      const yearRun = $(" div.subtext > a:nth-child(8)").text().trim();
       const runTime = $("time").contents().first().text().trim();
       //imdb rating
       const rating = $(".subtext").contents().first().text().trim();
-      const reviewRatingIMDB = $("div.imdbRating > div.ratingValue > strong > span").text().trim();
+      const reviewRatingIMDB = $(
+          "div.imdbRating > div.ratingValue > strong > span"
+        )
+        .text()
+        .trim();
       const ratingCountIMDB = $("div.imdbRating > a > span").text().trim();
       //metacritic score
-      const ratingMeta = $("div.titleReviewBar > div:nth-child(1) > a > div > span").text().trim();
-      //genres 
+      const ratingMeta = $(
+          "div.titleReviewBar > div:nth-child(1) > a > div > span"
+        )
+        .text()
+        .trim();
+      //genres
       const gernes = [];
       $("#titleStoryLine > div:nth-child(10) > a").map(function (i, el) {
         gerne = $(this).text().trim();
         gernes.push(gerne);
-
       });
-
-
+      //all the cast members
+      const cast = [];
+      $("#titleCast > table > tbody > tr> ").map(function (i, el) {
+        let castmember = $(this).find("td:nth-child(2) a").text().trim();
+        if (castmember != "") cast.push(castmember);
+      });
+      //poster might change it later
       const poster = $("div.poster a img").attr("src");
+      //summary text
       const summary = $("div.summary_text").text().trim();
+      //grab all the key words
 
-      const keywords = GetKeywords(imdbID);
-
+      //more like this
+      const similarIMDB = [];
+      $("#title_recs > div.rec_overviews > div>").each((i, el) => {
+        let title = $(el)
+          .find(
+            "div.rec_details > div > div.rec-jaw-upper > div.rec-title > a > b"
+          )
+          .text();
+        let id = $(el)
+          .find("div.rec_details > div > div.rec-jaw-upper > div.rec-title > a")
+          .attr("href");
+        if ((title != "") & (id != null)) {
+          const movie = {
+            title: title,
+            imdbID: id.match(/title\/(.*)\//)[1],
+          };
+          similarIMDB.push(movie);
+        }
+      });
+      const keywords = GetKeywords(imdbID).then(result => {
+        console.log(result);
+      });
       const movie = {
         title,
+        yearRun,
         rating,
         reviewRatingIMDB,
         ratingCountIMDB,
         ratingMeta,
-
         runTime,
         gernes,
         summary,
         poster,
-        keywords
+        keywords,
+        cast,
+        similarIMDB,
       };
       return movie;
     });
